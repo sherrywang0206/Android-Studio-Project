6a24c1593ae1b821808ea608343305fc86a581e7
finn3002 <neko06301202@gmail.com>
2020-05-27 20:05:09 +0800

    last week

--- app/src/main/java/com/team/team_project/MainActivity.java
+++ app/src/main/java/com/team/team_project/MainActivity.java
@@ -2,13 +2,25 @@ package com.team.team_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button loginbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginbutton  = findViewById(R.id.button2);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PageActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
